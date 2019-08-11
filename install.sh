02b768537fa3133b80a5e440a0e114ca94e680c7
WoodChip1650 <47008305+WoodChip1650@users.noreply.github.com>
2019-08-11 12:08:35 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,24 +22,39 @@ sudo apt-get install -y proftpd
 echo "<-- apache2 -->"
 sudo apt-get install -y apache2
 sudo echo "ServerName RaspiServ" >> /etc/apache2/httpd.conf
-
+wiat 5
+systemctl status apache2.service
+wait 10
+clear
 #PHP
 echo "<-- PHP -->"
-sudo apt-get install -y php5 libapache2-mod-php5 php5-intl php5-mcrypt php5-curl php5-gd php5-sqlite
-
+sudo apt-get install -y php libapache2-mod-php php-fpm 
+clear
 #MySQL
 echo "<-- Installing mySQL -->"
-sudo apt-get install -y mysql-server mysql-client php5-mysql
-
+sudo apt-get install -y mysql-server mysql-client mysql_secure_installation
+sudo mysql
+SELECT user,authentication_string,plugin,host FROM mysql.user;
+wait 30
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'T@nkD@gD@g';
+FLUSH PRIVILEGES;
+SELECT user,authentication_string,plugin,host FROM mysql.user;
+wait 15
+exit
+wait 5
+systemctl status mysql.service
+wait 10
+clear
 #Additional Dependencies
 echo "<-- Installing Additional Dependencies -->"
 sudo apt-get install -y nmap zenmap
-
+wiat 5
 #Webmin
 echo "<-- Installing Deps and Webmin -->"
+sudo mk /opt/install/Webmin/
 sudo apt install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python -y
 	sleep 20
-	wget http://www.webmin.com/download/deb/webmin-current.deb -O /opt/install/Webmin/webmin_all.deb
+	wget http://www.webmin.com/download/deb/webmin-current.deb  /opt/install/Webmin/webmin_all.deb
 	dpkg -i /opt/install/Webmin/webmin_all.deb
 	sleep 10
 	rm -rf /opt/install/Webmin/webmin_all.deb	
@@ -53,7 +68,14 @@ sudo apt install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runti
 	    if [ -f /etc/default/iptables ]; then
 	        sed -i "s/#-A INPUT -p tcp --dport 10000 -j ACCEPT/-A INPUT -p tcp --dport 10000 -j ACCEPT/g" /etc/default/iptables
 	        /etc/init.d/iptables restart;
+		fi
+clear
+systemctl status Webmin.service
+wait 10
+clear
 #myPHPAdmin Fixer
 curl -O -k https://raw.githubusercontent.com/skurudo/phpmyadmin-fixer/master/pma.sh && chmod +x pma.sh && ./pma.sh			
 #myPHPAdmin
 sudo apt install -y phpmyadmin
+wait 10
+exit
